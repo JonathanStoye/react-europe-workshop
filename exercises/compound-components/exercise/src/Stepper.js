a3010f62a6bc5998ec3ac9aa9b1ce87ef085b951
Jonathan Stoye <mail@jonathanstoye.de>
2018-05-15 13:05:20 +0200

    compund components

--- exercises/compound-components/exercise/src/Stepper.js
+++ exercises/compound-components/exercise/src/Stepper.js
@@ -1,4 +1,56 @@
-import React, { Component } from "react";
+import React, { Component, createContext } from "react";
+
+const { Provider, Consumer } = createContext()
+
+const Step = () => (
+  <Consumer>
+    {({ activeStep, hasNext, hasPrevious, stepBack, stepForward }) => (
+      <div className="step">
+        <h2>{activeStep.title}</h2>
+        <p>{activeStep.content}</p>
+        <div>
+          <button
+            className="step-btn"
+            disabled={!hasPrevious}
+            onClick={stepBack}
+          >
+            Previous
+            </button>
+          <button
+            className="step-btn"
+            disabled={!hasNext}
+            onClick={stepForward}
+          >
+            Next
+            </button>
+        </div>
+      </div>
+    )}
+  </Consumer>
+)
+
+const StepperStatus = () => (
+  <Consumer>
+    {({ steps, activeStepIndex, setActiveStep }) => (
+      <div className="stepper-status">
+        {steps.map((step, i) => (
+          <React.Fragment key={step.title}>
+            <span
+              className="status-item"
+              onClick={() =>
+                setActiveStep(i)
+              }
+              style={{ color: i === activeStepIndex ? "#000000" : "#aaaaaa" }}
+            >
+              {step.title}
+            </span>
+            {i < steps.length - 1 && <span className="status-divider" />}
+          </React.Fragment>
+        ))}
+      </div>
+    )}
+  </Consumer>
+)
 
 export default class Stepper extends Component {
   state = {
@@ -26,52 +78,37 @@ export default class Stepper extends Component {
     });
   };
 
-  render() {
+  setActiveStep = (stepIndex) => {
+    this.setState({
+      activeStepIndex: stepIndex
+    })
+  }
+
+  getContext = () => {
     const { steps } = this.props;
     const { activeStepIndex } = this.state;
     const activeStep = steps[activeStepIndex];
     const hasPrevious = activeStepIndex > 0;
     const hasNext = activeStepIndex < steps.length - 1;
+    return {
+      steps,
+      activeStepIndex,
+      activeStep,
+      hasPrevious,
+      hasNext,
+      stepBack: this.stepBack,
+      stepForward: this.stepForward,
+      setActiveStep: this.setActiveStep,
+    }
+  }
+
+  render() {
     return (
       <div className="stepper">
-        <div className="stepper-status">
-          {steps.map((step, i) => (
-            <React.Fragment key={step.title}>
-              <span
-                className="status-item"
-                onClick={() =>
-                  this.setState({
-                    activeStepIndex: i
-                  })
-                }
-                style={{ color: i === activeStepIndex ? "#000000" : "#aaaaaa" }}
-              >
-                {step.title}
-              </span>
-              {i < steps.length - 1 && <span className="status-divider" />}
-            </React.Fragment>
-          ))}
-        </div>
-        <div className="step">
-          <h2>{activeStep.title}</h2>
-          <p>{activeStep.content}</p>
-          <div>
-            <button
-              className="step-btn"
-              disabled={!hasPrevious}
-              onClick={this.stepBack}
-            >
-              Previous
-            </button>
-            <button
-              className="step-btn"
-              disabled={!hasNext}
-              onClick={this.stepForward}
-            >
-              Next
-            </button>
-          </div>
-        </div>
+        <Provider value={this.getContext()}>
+          <StepperStatus />
+          <Step />
+        </Provider>
       </div>
     );
   }
