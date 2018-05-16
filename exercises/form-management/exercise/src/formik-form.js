172d826e53070995cd4b20bf169f266da2f3a2cb
Jonathan Stoye <mail@jonathanstoye.de>
2018-05-16 11:28:25 +0200

    formik

--- exercises/form-management/exercise/src/formik-form.js
+++ exercises/form-management/exercise/src/formik-form.js
@@ -1,92 +1,144 @@
 import React, { Component } from "react";
-// import { Formik } from 'formik';
+import { Formik, Form, Field } from "formik";
 
 import "./formik-form.css";
 
 export class Content extends Component {
+  renderChoices = submission =>
+    Object.keys(submission).map(key => {
+      if (key !== "name") {
+        const value = submission[key] ? `✔️` : `⤬`;
+        return (
+          <p key={key}>
+            {key}: {value}
+          </p>
+        );
+      }
+      return null;
+    });
+
+  renderSubmissions = submissions => (
+    <ul>
+      {submissions.map((submission, index) => (
+        <li key={`${submission.name}${index}`}>
+          <p style={{ fontWeight: "bold" }}>{submission.name}</p>
+          {this.renderChoices(submission)}
+        </li>
+      ))}
+    </ul>
+  );
   render() {
-    return <div className="contentWrapper">We want to put form info here!</div>;
+    const { submissions } = this.props;
+
+    return (
+      <div className="contentWrapper">
+        {this.renderSubmissions(submissions)}
+      </div>
+    );
   }
 }
 
 export default class FormikForm extends Component {
-  constructor() {
-    super();
+  state = {
+    submissions: []
+  };
 
-    this.state = {
-      input: "",
-      react: false,
-      redux: false,
-      formik: false,
-      graphQL: false,
-      node: false,
-      reactNative: false
-    };
-  }
-
-  handleSubmit = () => {
-    console.log("submitted!");
+  handleSubmit = submission => {
+    this.setState({
+      submissions: this.state.submissions.concat(submission)
+    });
   };
 
   // we'd also have to have several onChange handlers and, of course, a more useful submit method!
 
   render() {
+    console.log(this.state.submissions);
     return (
       <div className="pageWrapper">
         <div className="formWrapper">
-          <form className="form" onSubmit={this.handleSubmit}>
-            <div>
-              <label htmlFor="name">What's your name?</label>
-              <input type="text" id="name" name="name" />
-            </div>
-            <div>
-              <p>Which of the following technologies have you used?</p>
-              <div>
-                <input type="checkbox" id="react" name="react" value="react" />
+          <Formik
+            onSubmit={(values, actions) => {
+              this.handleSubmit(values);
+              actions.resetForm();
+            }}
+            initialValues={{
+              name: "",
+              react: false,
+              formik: false,
+              redux: false,
+              grapql: false,
+              reactNative: false,
+              node: false
+            }}
+            render={({ values, handleChange }) => (
+              <Form className="form">
+                <label htmlFor="name">What's your name?</label>
+                <Field
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Your name"
+                  onChange={handleChange}
+                  value={values.name}
+                  required
+                />
+                <p>Which of the following technologies have you used?</p>
                 <label htmlFor="react">React</label>
-              </div>
-              <div>
-                <input type="checkbox" id="redux" name="redux" value="redux" />
-                <label htmlFor="redux">Redux</label>
-              </div>
-              <div>
-                <input
+                <Field
                   type="checkbox"
-                  id="formik"
-                  name="formik"
-                  value="formik"
+                  onChange={handleChange}
+                  id="react"
+                  name="react"
+                  checked={values.react}
                 />
                 <label htmlFor="formik">Formik</label>
-              </div>
-              <div>
-                <input
+                <Field
                   type="checkbox"
-                  id="graphQL"
-                  name="graphQL"
-                  value="graphQL"
+                  onChange={handleChange}
+                  id="formik"
+                  name="formik"
+                  checked={values.formik}
                 />
-                <label htmlFor="graphQL">GraphQL</label>
-              </div>
-              <div>
-                <input type="checkbox" id="node" name="node" value="node" />
-                <label htmlFor="node">Node</label>
-              </div>
-              <div>
-                <input
+                <label htmlFor="redux">Redux</label>
+                <Field
                   type="checkbox"
-                  id="reactNative"
-                  name="reactNative"
-                  value="reactNative"
+                  onChange={handleChange}
+                  id="redux"
+                  name="redux"
+                  checked={values.redux}
+                />
+                <label htmlFor="grapql">GraphQL</label>
+                <Field
+                  type="checkbox"
+                  onChange={handleChange}
+                  id="grapql"
+                  name="grapql"
+                  checked={values.graphql}
                 />
                 <label htmlFor="reactNative">ReactNative</label>
-              </div>
-            </div>
-            <button className="submitButton" type="submit">
-              Submit Form
-            </button>
-          </form>
+                <Field
+                  type="checkbox"
+                  onChange={handleChange}
+                  id="reactNative"
+                  name="reactNative"
+                  checked={values.reactNative}
+                />
+                <label htmlFor="node">Node</label>
+                <Field
+                  type="checkbox"
+                  onChange={handleChange}
+                  id="node"
+                  name="node"
+                  checked={values.node}
+                />
+                <button className="submitButton" type="submit">
+                  Submit Form
+                </button>
+              </Form>
+            )}
+          />
         </div>
-        <Content />
+        <Content submissions={this.state.submissions} />
       </div>
     );
   }
