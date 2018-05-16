7e81a116dad32a39baf3fd8460341a4e9cbb72a9
Jonathan Stoye <mail@jonathanstoye.de>
2018-05-16 12:57:37 +0200

    react-native

--- exercises/universal-components/exercise/src/index.js
+++ exercises/universal-components/exercise/src/index.js
@@ -1,21 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import "./index.css";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  PanResponder
+} from "react-native";
 
 class App extends React.Component {
+  state = {
+    background: "red",
+    x: 0,
+    y: 0
+  };
+  _panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: (evt, gestureState) => true,
+    onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onPanResponderGrant: (evt, gestureState) => {
+      this.setState({
+        background: "blue"
+      });
+    },
+    onPanResponderMove: (evt, gestureState) => {
+      this.setState(state => ({
+        x: gestureState.dx,
+        y: gestureState.dy
+      }));
+    },
+    onPanResponderTerminationRequest: (evt, gestureState) => true,
+    onPanResponderRelease: (evt, gestureState) => {
+      this.setState({
+        background: "red",
+        x: 0,
+        y: 0
+      });
+    },
+    onPanResponderTerminate: (evt, gestureState) => {},
+    onShouldBlockNativeResponder: (evt, gestureState) => {
+      return true;
+    }
+  });
+
   render() {
     return (
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello, world!</Text>
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.box,
+            {
+              alignSelf: "flex-start",
+              backgroundColor: "black",
+              transform: [
+                { translateX: this.state.x },
+                { translateY: this.state.y }
+              ]
+            }
+          ]}
+          {...this._panResponder.panHandlers}
+        />
+        <View style={[styles.box, { backgroundColor: "red" }]} />
+        <View
+          style={[
+            styles.box,
+            { alignSelf: "flex-end", backgroundColor: "gold" }
+          ]}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  box: { padding: 10 },
-  text: { fontWeight: 'bold' },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: "red"
+  }
 });
 
-AppRegistry.registerComponent('App', () => App);
-AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
+AppRegistry.registerComponent("App", () => App);
+if (Platform.OS === "web") {
+  AppRegistry.runApplication("App", {
+    rootTag: document.getElementById("root")
+  });
+}
